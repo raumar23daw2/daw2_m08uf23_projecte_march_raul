d08a1becc2daa8af886e61dec1974546f07b5580
ramave <456982.clot@fje.edu>
2024-04-03 19:17:24 +0200

    commit 1

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>
+			Projecte m08uf23
+		</title>
+	</head>
+	<body>
+		<h1> Aplicació d'accés a base de dades LDAP</h1>
+		<h2> DAW2 M08UF2 M08UF3 </h2>
+		<h3> Autor: ramave</h3>
+		<h3> Correu: 456982.clot@fje.edu</h3>
+		<h3> Github: https://github.com/raumar23daw2</h3>
+		<a href="http://zend-ramave.fjeclot.net/autent/login.php">Inicia sessió</a>
+	</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>
+			Gestio de base de dades LDAP
+		</title>
+	</head>
+	<body>
+		<h2> MENÚ PRINCIPAL DE GESTIO</h2>
+		<h3> <b>Visualitza</b> </h3>
+		<h3> <b>Afegeix</b> </h3>
+		<h3> <b>Esborra</b> </h3>
+		<h3> <b>Edita</b> </h3>
+		<a href="http://zend-ramave.fjeclot.net/autent/index.php">Torna a la pàgina inicial</a>
+	</body>
+</html>
